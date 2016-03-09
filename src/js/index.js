6e140fdda0b51759538e7215e7851df652c196f8
Mallerie Shirley <mallerie@gmail.com>
2016-03-09 07:36:51 -0500

    Initital Commit

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+// Javascript Entry Point
+
+var button1  = document.querySelector('.button1')
+var button2  = document.querySelector('.button2')
+var button3  = document.querySelector('.button3')
+
+var box1  = document.querySelector('.section-one')
+var box2  = document.querySelector('.section-two')
+var box3  = document.querySelector('.section-three')
+
+
+button2.addEventListener('click', function(event) {
+  
+    event.currentTarget.style('slid2');
+  
+});
+
+
